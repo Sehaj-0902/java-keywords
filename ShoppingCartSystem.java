2264010dd0a2956e26dc5d05dd77505e8108b1f0
Sehaj-0902 <sehajpreet1296.be21@chitkara.edu.in>
2025-03-12 11:05:52 +0530

    [Sehaj] Add: Program to display product details and update discount

--- ShoppingCartSystem.java
+++ ShoppingCartSystem.java
@@ -40,8 +40,15 @@ public class ShoppingCartSystem {
         Product product2 = new Product(102, "Smartphone", 45000, 2);
         product2.displayProductDetails();
 
-        Product.updateDiscount(20.0);
+        System.out.println("\nUpdated Product Details:");
+        product1.updateDiscount(15.0);
         System.out.println("New Discount Updated!!");
+        product1.displayProductDetails();
+
+        product2.updateDiscount(20.0);
+        System.out.println("New Discount Updated!!");
+        product2.displayProductDetails();
+
     }
 }
 
@@ -58,5 +65,18 @@ Output:
     Price: 45000.0
     Quantity: 2
     Discount: 10.0%
+
+    Updated Product Details:
     New Discount Updated!!
+    Product ID: 101
+    Name: Laptop
+    Price: 75000.0
+    Quantity: 1
+    Discount: 15.0%
+    New Discount Updated!!
+    Product ID: 102
+    Name: Smartphone
+    Price: 45000.0
+    Quantity: 2
+    Discount: 20.0%
  */
